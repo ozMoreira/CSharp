514370fc2df0aa2c5c0158a5f90480fb46db950b
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-04 17:37:29 -0300

    Adicionando exercicio de Calculo de Circunferencia com Classe Calculadora com membros estáticos

---
+++ Exercicios/Calc_Circulos_POO/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calc_Circulos_POO {
+    class Calculadora {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double r) {
+            return 2.0 * Pi * r;
+        }
+
+        public double Volume(double r) {
+            return 4.0 / 3 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
--- Exercicios/Calc_Circulos_POO/Program.cs
+++ Exercicios/Calc_Circulos_POO/Program.cs
@@ -3,25 +3,18 @@
 namespace Calc_Circulos_POO {
     class Program {
 
-        static double Pi = 3.14;
+        
         static void Main(string[] args) {
+
             Console.Write("Entre com o valor do Raio: >>> ");
             double raio = double.Parse(Console.ReadLine());
 
-            double circle = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circle = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferencia: " + circle.ToString("F2"));
             Console.WriteLine("Volume: " + volume.ToString("F2"));
-            Console.WriteLine("Pi: " + Pi);
-        }
-
-        static double Circunferencia(double r) {
-            return 2.0 * Pi * r;
-        }
-
-        static double Volume(double r) {
-            return 4.0 / 3 * Pi * Math.Pow(r, 3);
+            Console.WriteLine("Pi: " + Calculadora.Pi);
         }
 
     }
