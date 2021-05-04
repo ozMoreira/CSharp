465ef6e7d0684f6a55ee00a355d31799b2ea3c23
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-04 17:24:07 -0300

    Adicionando exercicio de Calculo de Circunferencia com Metodo na Classe Principal

---
+++ Exercicios/Calc_Circulos_POO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calc_Circulos_POO {
+    class Program {
+
+        static double Pi = 3.14;
+        static void Main(string[] args) {
+            Console.Write("Entre com o valor do Raio: >>> ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circle = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circle.ToString("F2"));
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Pi: " + Pi);
+        }
+
+        static double Circunferencia(double r) {
+            return 2.0 * Pi * r;
+        }
+
+        static double Volume(double r) {
+            return 4.0 / 3 * Pi * Math.Pow(r, 3);
+        }
+
+    }
+}
