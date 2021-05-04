b4007e5bd49743c8e13635f108353b3461a9b737
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-04 16:42:00 -0300

    Adicionando exercicio de Calculo de Aumento Salarial

---
+++ Exercicios/Aumento_Salarial_POO/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aumento_Salarial_POO {
+    class Funcionario {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double CalculaLiquido(double salarioLiquido) {
+            return salarioLiquido = SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double percentual) {
+            SalarioBruto += (SalarioBruto * 10) / 100;
+        }
+
+        public override string ToString() {
+            return "\n   Nome: " + Nome +
+                "\n   Salário Bruto: R$ " + SalarioBruto.ToString("F2") +
+                "\n   Impostos Descontados na Folha: R$ " + Imposto.ToString("F2") +
+                "\n   Salário Liquido: R$ " + CalculaLiquido(SalarioBruto).ToString("F2");
+        }
+    }
+}
---
+++ Exercicios/Aumento_Salarial_POO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aumento_Salarial_POO {
+    class Program {
+        static void Main(string[] args) {
+            Funcionario f = new Funcionario();
+
+            Console.WriteLine("Insira os dados do Funcionário");
+            Console.Write("Nome >>> ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto >>> R$ ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Impostos Descontados na Folha >>> R$ ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nFuncionario: " + f);
+
+            Console.Write("\nInforme o Percentual que o funcionário terá de aumento salarial: (%) >>> ");
+            double percent = double.Parse(Console.ReadLine());
+            f.AumentarSalario(percent);
+
+            Console.WriteLine("\nFuncionario atualizado: " + f);
+        }
+    }
+}
