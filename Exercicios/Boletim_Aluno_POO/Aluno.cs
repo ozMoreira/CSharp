43f4ed062842c895eddaf65d73a130006b3add0a
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-04 17:03:37 -0300

    Adicionando exercicio de Calculo de Aumento Salarial

---
+++ Exercicios/Boletim_Aluno_POO/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boletim_Aluno_POO {
+    class Aluno {
+
+        public string Nome;
+        public double N1, N2, N3;
+
+        public double MediaFinal() {
+            return ((N1 * 30) + (N2 * 35) + (N3 * 35)) / 100;
+        }
+
+        public bool Aprovado() {
+            if (MediaFinal() > 6.0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public double MargemFaltante() {
+            if (Aprovado()) {
+                return 0.0;
+            } else {
+                return 6.0 - MediaFinal();
+            }
+        }
+
+    }
+}
---
+++ Exercicios/Boletim_Aluno_POO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Boletim_Aluno_POO {
+    class Program {
+        static void Main(string[] args) {
+            
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Informe os dados do Aluno abaixo:");
+            Console.Write("Nome >>> ");
+            aluno.Nome = Console.ReadLine();
+            Console.Write("Nota do 1º Trim. >>> ");
+            aluno.N1 = double.Parse(Console.ReadLine());
+            Console.Write("Nota do 2º Trim. >>> ");
+            aluno.N2 = double.Parse(Console.ReadLine());
+            Console.Write("Nota do 3º Trim. >>> ");
+            aluno.N3 = double.Parse(Console.ReadLine());
+
+            if (aluno.Aprovado()) {
+                Console.WriteLine("\n\nAluno aprovado com sucesso!!!!");
+            } else {
+                Console.WriteLine("\n\nREPROVADO - Faltou " 
+                    + aluno.MargemFaltante().ToString("F2") + " pontos para aprovação!");
+            }
+        }
+    }
+}
