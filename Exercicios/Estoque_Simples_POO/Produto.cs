4b8e6eb76959a601609811fcd4c57a2f9a3f7070
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-05 09:47:35 -0300

    Criando Encapsulamento no Exercicio Produtos

--- Exercicios/Estoque_Simples_POO/Produto.cs
+++ Exercicios/Estoque_Simples_POO/Produto.cs
@@ -1,34 +1,50 @@
 ﻿namespace Estoque_Simples_POO {
     class Produto {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
-        public Produto(string nome, double preco, int quantidade) {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+        public Produto() {
         }
 
-        public Produto(string nome, double preco) {
-            Nome = nome;
-            Preco = preco;
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string GetNome() {
+            return _nome;
+        }
+
+        public int getQuantidade() {
+            return _quantidade;
+        }
+
+        public void SetNome(string nome) {
+            if (nome != null && nome.Length > 1) { 
+            _nome = nome;
+            }
+        }
+
+        public double GetPreco() {
+            return _preco;
         }
 
         public double ValorTotalEmEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
         public void AdicionaProdutos(int quantidade) {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoveProdutos(int quantidade) {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
         public override string ToString() {
-            return Nome +
-                "\nPreço Unitário: R$ " + Preco.ToString("F2") +
-                "\nQuantidade Disponível: " + Quantidade +
+            return _nome +
+                "\nPreço Unitário: R$ " + _preco.ToString("F2") +
+                "\nQuantidade Disponível: " + _quantidade +
                 "\nValor Total do Estoque: R$ " + ValorTotalEmEstoque().ToString("F2");
         }
     }
