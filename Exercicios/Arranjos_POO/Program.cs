1a6d69511cde45c40a6aa692fa0e84aedc331c58
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-06 10:57:37 -0300

    Inserindo Exemplo de Vetores

---
+++ Exercicios/Arranjos_POO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arranjos_POO {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite o número de lançamentos que deseja realizar: >>> ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[3];
+
+            for (int i = 0; i < n; i++) {
+                Console.Write("Informe a altura >>> ");
+                vect[i] = double.Parse(Console.ReadLine());
+            }
+            double sum = 0;
+            for (int i = 0; i < vect.Length; i++) {
+                sum += vect[i];
+            }
+            double avg = sum / n;
+            Console.WriteLine("Média das alturas é de " + avg.ToString("f2"));
+        }
+    }
+}
