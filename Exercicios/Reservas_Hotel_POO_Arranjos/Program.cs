494aba8fd39a4d80690722769f3ab1e0cb14025a
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-06 16:04:20 -0300

    Criando exercicio de Reserva de Hotel

---
+++ Exercicios/Reservas_Hotel_POO_Arranjos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Reservas_Hotel_POO_Arranjos {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Informe o número de quartos para ocupar: >>> ");
+            int quartos = int.Parse(Console.ReadLine());
+            Reserva[] resV = new Reserva[10];
+            if (quartos <= 10) {
+                for (int i = 0; i < quartos; i++) {
+                    int saldo = quartos - i;
+                    Console.WriteLine("Faltam ainda " + saldo + " lançamentos!!!");
+                    Console.Write("\nNome do Hospede: >>> ");
+                    string hospede = Console.ReadLine();
+
+                    Console.Write("E-mail de contato: >>> ");
+                    string emailContato = Console.ReadLine();
+
+                    Random rd = new Random();
+                    int x = rd.Next(10);
+
+                    if (resV[x] == null) {
+                        resV[x] = new Reserva(i, hospede, emailContato, x);
+                    } else {
+                        x = rd.Next(10);
+                        while (resV[x] != null) {
+                            x = rd.Next(10);
+                        }
+                        resV[x] = new Reserva(i, hospede, emailContato, x);
+                    }
+                    Console.Clear();
+                }
+            } else {
+                Console.WriteLine("\n\nNúmero de Quartos desejados Maior que o Disponível");
+            }
+
+            for (int p = 0; p < 10; p++) {
+                if (resV[p] != null) {
+                    Console.WriteLine(resV[p]);
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Exercicios/Reservas_Hotel_POO_Arranjos/Reserva.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservas_Hotel_POO_Arranjos {
+    class Reserva {
+
+        public int NroReserva { get; private set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int NroQuarto { get; private set; }
+
+        public Reserva(int reserva, string nome, string email, int quarto) {
+            this.NroReserva = reserva;
+            this.Nome = nome;
+            this.Email = email;
+            this.NroQuarto = quarto;
+        }
+
+        public Reserva(int quarto) {
+            this.NroQuarto = quarto;
+        }
+        /*public int Buscador(int quartos) {
+            if (resV.)
+            return 
+        }*/
+        public override string ToString() {
+            return "\n\nBooking # " + NroQuarto +
+                "\nHóspede Principal: " + Nome +
+                "\nE-mail: " + Email +
+                "\nQuarto # " + NroQuarto;
+        }
+
+    }
+}
