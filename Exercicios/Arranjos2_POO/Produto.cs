90463c16425c44f88d365c9c680cbb2c48f0474f
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-06 12:36:59 -0300

    Incluindo Classe Produto no exemplo de Arranjos2

---
+++ Exercicios/Arranjos2_POO/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arranjos2_POO {
+    class Produto {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Produto(string name, double price) {
+            this.Name = name;
+            this.Price = price;
+        }
+
+    }
+}
--- Exercicios/Arranjos2_POO/Program.cs
+++ Exercicios/Arranjos2_POO/Program.cs
@@ -5,22 +5,24 @@ namespace Arranjos2_POO {
         static void Main(string[] args) {
             Console.Write("Informe o número de produtos que deseja cadastrar >>> ");
             int n = int.Parse(Console.ReadLine());
-            double[] pVect = new double[n];
+
+            Produto[] pVect = new Produto[n];
 
             for (int i = 0; i < n; i++) {
                 Console.Write("\nQual o nome do produto  >>> ");
-                string prod = Console.ReadLine();
-                Console.Write("Informe o Preço de " + prod + " >>> R$ ");
-                pVect[i] = double.Parse(Console.ReadLine());
+                string name = Console.ReadLine();
+                Console.Write("Informe o Preço de " + name + " >>> R$ ");
+                double price = double.Parse(Console.ReadLine());
+                pVect[i] = new Produto(name, price);
             }
 
             double somaP = 0;
             for (int i = 0; i < pVect.Length; i++) {
-                somaP += pVect[i];
+                somaP += pVect[i].Price;
             }
 
             double avg = somaP / n;
-            Console.WriteLine("\n\nA média do preço dos produtos " + avg.ToString("F2"));
+            Console.WriteLine("\n\nA média do preço dos produtos é R$ " + avg.ToString("F2"));
         }
     }
 }
