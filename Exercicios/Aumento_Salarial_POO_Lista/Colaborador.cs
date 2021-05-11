b25ec5835568a32a38d1faafb7f0261a8256ca23
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-11 15:21:42 -0300

    Finalizado Sistema de Lançamento de Aumento Salarial com busca na Lista por ID

---
+++ Exercicios/Aumento_Salarial_POO_Lista/Colaborador.cs
@@ -0,0 +1,23 @@
+﻿namespace Aumento_Salarial_POO_Lista {
+    class Colaborador {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Colaborador(int id, string nome, double salario) {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void aumentaSalario(double percentual) {
+            Salario += ((Salario * percentual) / 100);
+        }
+
+      
+        public override string ToString() {
+            return Id + ", " + Nome + ", " + Salario.ToString("F2");
+        }
+    }
+}
---
+++ Exercicios/Aumento_Salarial_POO_Lista/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aumento_Salarial_POO_Lista {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Informe um número de colaboradores para lançamento >>> ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Colaborador> listQuadro = new List<Colaborador>();
+
+            for (int i = 1; i <= n; i++) {
+                Console.Write("\nId: >>> ");
+                int id = int.Parse(Console.ReadLine());
+                Colaborador validaId = listQuadro.Find(x => x.Id == id);
+                while (validaId != null) {
+                    Console.Clear();
+                    Console.WriteLine("ID EXISTENTE!");
+                    Console.Write("\nId: >>> ");
+                    id = int.Parse(Console.ReadLine());
+                    validaId = listQuadro.Find(x => x.Id == id);
+                }
+                Console.Write("Colaborador: >>> ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario >>> R$ ");
+                double salario = double.Parse(Console.ReadLine());
+
+                listQuadro.Add(new Colaborador(id, nome, salario));
+                Console.Clear();
+            }
+
+            Console.Write("Informe o ID do Colaborador que receberá aumento: >>> ");
+            int recAumentoID = int.Parse(Console.ReadLine());
+            Colaborador pegaId = listQuadro.Find(x => x.Id == recAumentoID);
+            Console.WriteLine(pegaId);
+            if (pegaId == null) {
+                while (pegaId == null) {
+                    Console.Clear();
+                    Console.WriteLine("ID EXISTENTE!");
+                    Console.Write("\nId: >>> ");
+                    recAumentoID = int.Parse(Console.ReadLine());
+                    pegaId = listQuadro.Find(x => x.Id == recAumentoID);
+                }
+            } 
+            if (pegaId != null) {
+                Console.Clear();
+                Console.Write("Informe o percentual de reajuste do salario: >>> ");
+                double reajuste = double.Parse(Console.ReadLine());
+                pegaId.aumentaSalario(reajuste);      
+            }
+
+            Console.WriteLine("\n Quadro de Colaboradores:");
+            foreach (Colaborador obj in listQuadro) {
+                Console.WriteLine(" " + obj);
+            }
+        }
+    }
+}
+
