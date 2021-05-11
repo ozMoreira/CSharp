9e3d764fd0416d36d63d65ee1aa38b4f9817def3
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-11 15:44:57 -0300

    Incluindo exemplo de matrizes

---
+++ Exercicios/Matrizes_POO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Matrizes_POO {
+    class Program {
+        static void Main(string[] args) {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length);
+
+            Console.WriteLine(mat.Rank);
+
+            Console.WriteLine(mat.GetLength(0));
+
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
