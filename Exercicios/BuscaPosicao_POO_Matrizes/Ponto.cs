4957b47ae2fa8051e81635aa2e670c29953ec75f
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-12 10:55:39 -0300

    Inserindo Exercicio de Matrizes - Trabalhando com indices

---
+++ Exercicios/BuscaPosicao_POO_Matrizes/Ponto.cs
@@ -0,0 +1,25 @@
+﻿namespace BuscaPosicao_POO_Matrizes {
+    class Ponto {
+
+        public int Point { get; set; }
+        public int Pup { get; set; }
+
+        public int PRight { get; set; }
+
+        public int PDown { get; set; }
+
+        public int PLeft { get; set; }
+
+        public Ponto(int point, int pup, int pRight, int pDown, int pLeft) {
+            Point = point;
+            Pup = pup;
+            PRight = pRight;
+            PDown = pDown;
+            PLeft = pLeft;
+        }
+
+        public override string ToString() {
+            return base.ToString(); 
+        }
+    }
+}
---
+++ Exercicios/BuscaPosicao_POO_Matrizes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BuscaPosicao_POO_Matrizes {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Insira o Número de Linhas e Colunas da Matriz, separados por um espaço: >>> ");
+            string[] dimensao = Console.ReadLine().Split(' ');
+            int[,] matriz = new int[int.Parse(dimensao[0]), int.Parse(dimensao[1])];
+
+            for (int l = 0; l < int.Parse(dimensao[0]); l++) {
+                Console.Write("Insira uma sequencia de " + dimensao[1] + " números separados por espaço: >>> ");
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int c = 0; c < int.Parse(dimensao[1]); c++) {
+                    matriz[l, c] = int.Parse(valores[c]);
+                }
+            }
+            Console.WriteLine("----------------------------------------------------");
+            Console.Write("\n\n Informe um número da Matriz, para saber seus vizinhos: >>> ");
+            int buscado = int.Parse(Console.ReadLine());
+
+            for (int l = 0; l < int.Parse(dimensao[0]); l++) {
+                for (int c = 0; c < int.Parse(dimensao[1]); c++) {
+                    
+                    if (matriz[l, c] == buscado) {
+                        Console.WriteLine("\nEncontrado na posição " + l + "," + c);
+                        int x = l - 1;
+                        if (x >= 0) {
+                            Console.WriteLine("Up: " + matriz.GetValue(x, c));
+                        }
+                        x = c + 1;
+                        if (x <= int.Parse(dimensao[0])) {
+                            Console.WriteLine("Right: " + matriz.GetValue(l, x));
+                        }
+                        x = l + 1;
+                        if (x <= int.Parse(dimensao[1])) {
+                            Console.WriteLine("Down: " + matriz.GetValue(x, c));
+                        }
+                        x = c - 1;
+                        if (x >= 0) {
+                            Console.WriteLine("left: " + matriz.GetValue(l, x));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
