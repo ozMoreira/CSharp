ee25d3f32362d661178413d39564d481921a47b0
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-12 11:33:21 -0300

    Incluindo expressao condicional ternaria

---
+++ Exercicios/Expressao_Condicional_Ternaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Expressao_Condicional_Ternaria {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Informe um preço qualquer: >>> R$ ");
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = (preco < 20.0) ? desconto = preco * 0.1 : desconto = preco * 0.5;
+            Console.WriteLine(desconto);
+        }
+    }
+}
