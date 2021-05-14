093d768e86028ce3faf00b2efb28915cb213a3ad
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-14 13:25:00 -0300

    Finalizando Exercicio Controle de Pedidos

--- Exercicios/Controle_De_Pedidos/Entities/Pedido.cs
+++ Exercicios/Controle_De_Pedidos/Entities/Pedido.cs
@@ -33,5 +33,46 @@ namespace Controle_De_Pedidos.Entities
         {
             Item.Remove(itemPedido);
         }
+
+        public double Total()
+        {
+            double soma = 0;
+            foreach (ItemPedido item in Item)
+            {
+                soma += item.SubTotal();
+            }
+            return soma;
+        }
+        public override string ToString()
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            Console.Clear();
+            sb.AppendLine("\n\n>>> RESUMO DO PEDIDO <<<");
+            sb.Append("\nInstante do Pedido: ");
+            sb.AppendLine(Instante.ToString("dd/MM/yyy HH:mm:ss"));
+            sb.Append("Status do Pedido: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Cliente: ");
+            sb.Append(Cliente.Nome);
+            sb.Append(" (");
+            sb.Append(Cliente.DataNascimento.ToString("dd/MM/yyy"));
+            sb.Append(" ) - ");
+            sb.AppendLine(Cliente.Email);
+            sb.AppendLine("\nItens do Pedido");
+            foreach (ItemPedido i in Item)
+            {
+                sb.Append(i.Produto.Nome);
+                sb.Append(", R$ ");
+                sb.Append(i.Preco.ToString("F2"));
+                sb.Append(", Quantidade: ");
+                sb.Append(i.Quantidade);
+                sb.Append(", Subtotal: R$ ");
+                sb.AppendLine(i.SubTotal().ToString("F2"));
+                sb.Append("Valor Total: R$ ");
+                sb.AppendLine(Total().ToString("F2"));
+            }
+            return sb.ToString();
+        }
     }
 }
--- Exercicios/Controle_De_Pedidos/Program.cs
+++ Exercicios/Controle_De_Pedidos/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controle_De_Pedidos.Entities;
+using Controle_De_Pedidos.Entities.Enums;
+using System;
 
 namespace Controle_De_Pedidos
 {
@@ -6,7 +8,41 @@ namespace Controle_De_Pedidos
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("------ SEJA BEM VINDO AO SISTEMA DE PEDIDOS ------");
+            Console.WriteLine("\nEntre com os dados do Cliente");
+            Console.Write("Nome >>> ");
+            string tNome = Console.ReadLine();
+            Console.Write("Email >>> ");
+            string tEmail = Console.ReadLine();
+            Console.Write("Data de Nascimento (DD/MM/AAA) >>> ");
+            DateTime tNascimento = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEntre com os dados do Pedido");
+            Console.Write("Status >>> ");
+            StatusPedido tStatus = Enum.Parse<StatusPedido>(Console.ReadLine());
+
+            Cliente c = new Cliente(tNome, tEmail, tNascimento);
+            Pedido p = new Pedido(DateTime.Now, tStatus, c);
+
+            Console.Write("\n Quantidade de Itens do Pedido >>> ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("----ITENS DO PEDIDO----");
+                Console.Write($"\nProduto {i}>>> ");
+                string tProduto = Console.ReadLine();
+                Console.Write("Preço >>> R$ ");
+                double tPreco = double.Parse(Console.ReadLine());
+                Console.Write("Quantidade >>> ");
+                int tQtd = int.Parse(Console.ReadLine());
+
+                Produto prod = new Produto(tProduto, tPreco);
+                ItemPedido iP = new ItemPedido(prod, tQtd, tPreco);
+                p.AddItem(iP);
+                
+            }
+            Console.WriteLine(p);
         }
     }
 }
