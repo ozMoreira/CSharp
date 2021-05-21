e507f02e080573a8cf10bbf1013af5d2bec7ab89
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-21 11:20:39 -0300

    Inserindo exemplo de consultas da classe Path

---
+++ Exercicios/Arquivo5_Path/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivo5_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\tmp\DirectoryTest\file1.txt";
+
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+
+        }
+    }
+}
---
+++ Exercicios/Arquivos_Directory_DirecInfo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+
+namespace Arquivos_Directory_DirecInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\tmp\DirectoryTest";
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("\nFILES: ");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                Directory.CreateDirectory(path + @"\newFolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERRO: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
