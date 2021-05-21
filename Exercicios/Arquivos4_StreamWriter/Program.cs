a8760c47cd1893c0443395803774972eb5cd24a3
Oswaldo Moreira <oz.moreira@icloud.com>
2021-05-21 10:50:38 -0300

    Inserindo exemplo de Stream Writer

---
+++ Exercicios/Arquivos4_StreamWriter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos4_StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\tmp\file1.txt";
+            string targetPath = @"c:\tmp\file3.txt";
+             
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERRO: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
